2565044db2561dc7372d4e9d2e68c793b990d955
IshaBharti <65011273+IshaBharti@users.noreply.github.com>
2020-06-06 21:43:47 +0530

    listque

--- string.py
+++ string.py
@@ -1,14 +1,23 @@
-m='The quick brown fox jump over the lazy dog'
-rep='over'
-rep1='on'
-j=" "
-a=m.split()
+list=[1,2,34,5,4,3,4,33]
+list1=[8,6,5,4,3,42,2,5]
+dup=[]
 i=0
-while i<len(a):
-    if a[i]=="over":
-        j=j+"on"+" "
-    else:
-        j=j+m+" "
-    i=i+1
-print(j)
-        
+while i<len(list):
+    j=0
+    while j<len(list1):
+        if list[i] in list1:
+            dup.append(list[i])
+        j=j+1
+        i=i+1
+print(dup)
+a=0
+b=[]
+while a<len(dup):
+    c=(dup[a]*dup[a])
+    b.append(c)
+
+    a=a+1
+print(b)
+
+
+    
