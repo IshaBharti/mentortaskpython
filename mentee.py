1c2722e9d8a57f679a1ccbd45149007a67f6857a
IshaBharti <65011273+IshaBharti@users.noreply.github.com>
2020-06-10 12:09:05 +0530

    mentee

---
+++ mentee.py
@@ -0,0 +1,16 @@
+day=input("enter day")
+meal=input("enter time")
+if day=="monday" and meal=="breakfast":
+    print("poori sbji")
+elif day=="monday" and meal=="lunch":
+    print("daal chawal")
+elif day=="monday" and meal=="dinner":
+    print("chicken rice")
+elif day=="tuesday" and meal=="breakfast":
+    print("tea and toast")
+elif day=="tuesday" and meal=="lunch":
+     print("sambhar rice")
+elif day=="tuesday" and meal=="dinner":
+    print("manchuriyan")
+
+
