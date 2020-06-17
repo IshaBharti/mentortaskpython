0df5cb7868d135130d8ebee1736e66cbeac14682
IshaBharti <65011273+IshaBharti@users.noreply.github.com>
2020-06-17 10:35:42 +0530

    function

---
+++ fun.py
@@ -0,0 +1,11 @@
+#in function if else
+
+
+def number(a,b):
+    if b%a==0:
+        return ("it is divisible")
+    elif b+a==10:
+        return ("b and a is equal 10")
+    else:
+        return("wrong")
+print(number(3,7))
