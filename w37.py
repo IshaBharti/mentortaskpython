5c9d3abaf3f139618a0827047aa4eea335aa8595
IshaBharti <65011273+IshaBharti@users.noreply.github.com>
2020-08-07 21:53:02 +0530

    Delete w37.py

--- w37.py
+++
@@ -1,4 +0,0 @@
-8. Write a Python program to display the first and last colors from the following list. Go to the editor
-
-color_list = ["Red","Green","White" ,"Black"]
-print("The first color is:=",color_list[0],"\n","The Last color is:=",color_list[-1])
