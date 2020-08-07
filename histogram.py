63b9f30dcc3fba550f359ea33cdb0c8d80fbdd9c
IshaBharti <65011273+IshaBharti@users.noreply.github.com>
2020-08-07 21:54:24 +0530

    Update and rename wr14.py to histogram.py

---
+++ histogram.py
@@ -0,0 +1,5 @@
+list=[1,2,3,4]
+i=0
+while i<len(list):
+    i=i+1
+    print(i*"*")
--- wr14.py
+++
@@ -1,6 +0,0 @@
-26. Write a Python program to create a histogram from a given list of integers. Go to the editor
-list=[1,2,3,4]
-i=0
-while i<len(list):
-    i=i+1
-    print(i*"*")
