4c4a771b05445c23146ddd42c6c1080039dd323c
IshaBharti <65011273+IshaBharti@users.noreply.github.com>
2020-08-08 10:31:10 +0530

    Delete wr31.py

--- wr31.py
+++
@@ -1,9 +0,0 @@
-Write a Python function that takes a sequence of numbers and determines whether all the numbers are different from each othe
-
-
-def check(data):
-    if len(data)==len(set(data)):
-        return"True"
-    else:
-        return"false";
-print(check([2,4,5,7,9]))
